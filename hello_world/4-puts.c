a8247f7e111a04e708d0c78859f2708e18bd47e1
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-09-29 17:38:38 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main -Entry point
+ * Return: Always 0 (Succes)
+ */
+
 int main(void)
 {
-	puts("Programming is like building a miltilingual puzzle");
+	puts("\"Programming is like building a miltilingual puzzle");
 	return (0);
 }
