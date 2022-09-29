ae89f074181331b138599278f34c293c4c5b1204
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-09-29 18:42:21 +0000

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stidio.h>
+
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of an char: %zu byte(s)", sizeof(a));
+	printf("Size of a int: %zu byte(s)", sizeof(b));
+	printf("Size of a long int: %zu byte(s)", sizeof(c));
+	printf("Size of a long long int: %zu byte(s)", sizeof(d));
+	printf("Size of a float: %zu byte(s)", sizeof(e));
+	return (0);
