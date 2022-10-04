c50587066879d6b9effce8fbe25fdb4beb80d4ab
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-10-04 17:33:05 +0000

    3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -10,8 +10,10 @@ int main(void)
 	char abc;
 
 	for (abc = 'a'; abc <= 'z'; abc++)
+		putchar(abc);
 	for (abc = 'A'; abc <= 'Z'; abc++)
-	putchar(abc);
+		putchar(abc);
+
 	putchar('\n');
 
 	return (0);
