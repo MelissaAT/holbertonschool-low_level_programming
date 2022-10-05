513bb7b9db7b86ef17e814b02b23dffcb7f70c3a
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-10-05 17:38:12 +0000

    rename

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+	int single;
+
+	for (single = 0; single < 10; single++)
+		putchar((single % 10) + '0');
+	return (0);
+}
