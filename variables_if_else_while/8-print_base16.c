fa217bb584e93c10160ed4c2ae220980637c96b8
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-10-05 17:36:23 +0000

    9-pritn_comb.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -11,15 +11,12 @@ int main(void)
 	int low;
 
 	for (hex = '0'; hex <= '9'; hex++)
+	putchar(hex);
 	
-		putchar(hex);
-	
-
 	for (low = 'a'; low <= 'f'; low++)
 		putchar(low);
 	
 	putchar('\n');
-
 	return (0);
 
 }
