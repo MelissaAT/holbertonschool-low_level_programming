9aa8fe66ac6361a1ee5c78480129318f5dd9754e
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-10-05 17:47:15 +0000

    fix code

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,13 +10,15 @@ int main(void)
 	int single;
 
 	for (single = 0; single <= 9; single++)
+	{
 		putchar((single % 10) + '0');
 	if (single == 9)
-		
+		continue;
+
 	putchar(',');
 	putchar(' ');
 
-
+	}
 	putchar('\n');
 
 	return (0);
