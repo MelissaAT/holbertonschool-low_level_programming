dbe135931cbf217e3d04927d4f4fefad14062670
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-10-06 01:34:17 +0000

    fix space

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
  * print_sign - prints the sing of a number
  *
  * n: variable
- * Return: 1 and prints + if n is greater than zero, 0 and prints 0 if n is zero
+ * Return: 1 prints + if n is greater than zero, 0  prints 0 if n is zero
  */
 
 int print_sign(int n)
