52e067f7ee3aecb664dfc4fbcd450dd206dc95e7
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-10-07 18:07:22 +0000

    7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -4,21 +4,22 @@
  */
 void print_line(int n)
 {	
-	int s;
+	int s, d;
+	
 
 	if (n <= 0)
 	{
 		_putchar('\n');
+	}
+	else
+	for (d = 0; d < n; d++)
+	{ 
+		for (s = 0; s < n; s++)
+		{_putchar(32);
+                _putchar(92);
 
-	for (n = 0; n <= 0; n++);
+	}
 	}
 
-	else if
-	{
-	for (s = 0; s > n; s++)	
-	}	
-		_putchar(32);
-		_putchar(92);
-	
+	_putchar('\n');
 }
-
