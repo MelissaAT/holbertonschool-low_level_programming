aec2b0019f0560dcfa20445f0714054bb9378236
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-10-07 02:46:46 +0000

    5-more_numbers

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,13 @@
 #include "main.h"
 /**
- *
+ * more_numbers - prints 10 times the numbers
  */
-void more_numbers(void);
-
+void more_numbers(void)
+{
+	int n = 14, m;
+	
+	for (m = 0; m <= n; m++)
+	{
+	_putchar('\n');
+	}
+}
