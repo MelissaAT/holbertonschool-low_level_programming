c3e1be6ecc4f39b787374b8dda207b240c06b7c5
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-10-08 01:55:08 +0000

    fixing code

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -7,16 +7,17 @@ void print_square(int size)
 {
 	int i, j;
 
-	if (size > 0)
+	if (size <= 0)
 	{
+		_putchar('\n');
+	}
 		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j < i; j++)
+			for (j = 0; j < size; j++)
+			{
 				_putchar('#');
-
-				if (i < size)
-					_putchar('\n');
 			}
+					_putchar('\n');
 	}
 	_putchar('\n');
 }
