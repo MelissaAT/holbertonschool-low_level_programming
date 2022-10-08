df525b956f610dab392bebfc4a12eff03ceb3e68
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-10-08 01:27:12 +0000

    fix code

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,26 +1,23 @@
 #include "main.h"
 /**
- * print_diagonal - draws a diagonal lin
+ * print_diagonal - draws a diagonal line
+ * @n: Variable
  */
 void print_diagonal(int n)
-{	
-	int s, d;
-	
+{
+	int d, s;
 
 	if (n <= 0)
 	{
-		for (d = 0; d < n; d++)
 		_putchar('\n');
 	}
-	else
 	for (d = 0; d < n; d++)
-	{ 
-		for (s = 0; s < n; s++)
-		{_putchar(32);
-                _putchar(92);
-
+	{
+		for (s = 0; s < d; d++)
+			if (s < d)
+			_putchar(32);
+		_putchar(92);
 	}
-	}
-
 	_putchar('\n');
+
 }
