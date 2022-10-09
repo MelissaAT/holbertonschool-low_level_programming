3cd4e8b184ecf6610d4455fc22d6af2f28ba2e25
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-10-09 00:14:57 +0000

    fix whitespace

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
+ *
+ * Return: Always (Success)
  */
-int main (void)
+int main(void)
 {
 	int i;
-	
-	for ( i = 1; i <= 100; i++)
+
+	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 15) == 0)
-			printf ("FizzBuzz ");
+			printf("FizzBuzz");
+
 		else if (i % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
+
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
+
 		else
 			printf("%d", i);
-		if (i == 100)
-		printf (" ");
-	}
 
+		if (i == 100)
+			continue;
+		printf(" ");
+	}
 	return (0);
 }
