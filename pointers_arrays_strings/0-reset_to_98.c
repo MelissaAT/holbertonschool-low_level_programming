db4f37972c8895e50d7b43f9a84255b7562d5056
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-10-10 13:53:25 +0000

    fixing white space

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * reset_to_98 - that takes a pointer to an int as parameter and updates the value 
+ * reset_to_98 - that takes a pointer to an int as parameter
  *
  */
 void reset_to_98(int *n)
