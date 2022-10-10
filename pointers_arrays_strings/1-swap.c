3d37ebdc54d40ad239af15416e29838767dea9ab
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-10-10 14:18:29 +0000

    swap a and b

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swaps the value of two integers
+ * @a: variable 1
+ * @b: variable 2
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
