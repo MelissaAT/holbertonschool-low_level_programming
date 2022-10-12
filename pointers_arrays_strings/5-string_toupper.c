d5d71819c20f9790dbcdc121ead347a09516560f
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-10-12 18:26:09 +0000

    5-string_toupper.c

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - hanges all lowercase letters of a string to uppercase
+ */
+char *string_toupper(char *s)
+{
+	int i;
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] -32;
+		}
+	}
+	return (0);
+}
+
