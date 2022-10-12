c8a59bfc612e2e44f937fa252f419b9023d82011
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-10-12 15:18:25 +0000

    3-strcmp.c

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: 
+ * @s2:
+ * Return: 
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, f = 0;
+
+	while (f == 0)
+	{
+
+	if (s1[i] < s2[i])
+			f = -1;
+
+	else if (s1[i] > s2[i])
+		f = 1;
+
+	if (s1[i] == '\0')
+		break;
+	i++;
+	}
+	return (f);
+}
