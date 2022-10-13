d202c5bce74a89c6d9994da82e0df9a56c9747d8
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-10-13 01:21:55 +0000

    6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * cap_string - capitalize all words
+ * @s: string
+ * Return: s
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (i == 0)
+		{
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] = s[i] - 32;
+			}
+		}
+
+		else if (s[i - 1] == ' ')
+		{
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] = s[i] - 32;
+			}
+		}
+	}
+	return (s);
+}
