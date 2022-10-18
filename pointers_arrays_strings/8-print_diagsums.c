508c4a3eaddabef6a087ef04e724257fd2bccb5d
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-10-18 00:04:27 +0000

    8-print_diagsums.c

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -7,17 +7,11 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, p, l = 0, r = 0;
-
+	int i, j, l;
 	for (i = 0; i < size; i++)
 	{
-		p = (i * size) + i;
-		l += *(a + p);
+		j += *(a + (i * size + i));
+		l += *(a + (i * size + (size - i - 1)));
 	}
-	for (j = 0; j < size; j++)
-	{
-		p = (j * size) + (size - j);
-		r += *(a + p);
-	}
-	printf("%d, %d\n", l, r);
+	printf("%d, %d\n", j, l);
 }
