9888ea87c565084c20a3abbd1be202fe163838b3
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-10-19 23:27:59 +0000

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 	printf("argc: %d\n", argc);
 
 	for (i = 0; i < argc; i++)
-		printf("argv[%d] = %s\n", i, argv[0]);
+		printf("%s\n", argv[i]);
 
 	return (0);
 }
