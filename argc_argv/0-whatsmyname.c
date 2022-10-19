a71b40af2c24e0407db14aa61a33b6e713268345
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-10-19 23:06:43 +0000

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,15 +2,17 @@
 /**
  * main - Entry point
  * @argc: counts arguments
- * argv: s array of character pointers listing all the arguments
- * Return: Always (success) 
+ * argv[]: s array of character pointers listing all the arguments
+ * Return: Always (success)
  */
 int main(int argc, char *argv[])
 {
+	int i;
+
 	printf("argc: %d\n", argc);
 
-	for (int i = 0; i <argc; i++)
+	for (int i = 0; i < argc; i++)
 		printf("argv[%d] = %s\n", i, argv[i]);
-	
+
 	return (0);
 }
