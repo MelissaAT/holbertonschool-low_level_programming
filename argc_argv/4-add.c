09fe2fbdce0a04e710ec8fc64079001400c0a4f8
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-10-22 01:50:47 +0000

    4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum;
+	sum = 0;
+	
+	for (i = 1; i < argc; i++)
+	{
+		a+= atoi(argv[i];
+
+		if (a == 199)
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", a);
+	return (0);
+}
+
