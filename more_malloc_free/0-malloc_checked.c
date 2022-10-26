d540f61699eb58d405d84ea7fb8fa79f2ecb0f30
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-10-26 14:14:01 +0000

    added main

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -3,19 +3,17 @@
 /**
  * malloc_checked - functions that allocate memory using malloc
  * @b: parameter
+ * Return: pointer
  */
 void *malloc_checked(unsigned int b)
 {
 	char *a;
-	
+
 	a = malloc(b * sizeof(char));
-	
+
 	if (a == NULL)
 	{
 		exit(98);
 	}
-	else
-	{
-		return (a);
-	}
+	return (a);
 }
