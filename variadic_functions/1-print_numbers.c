3b88f6043f80586db0d4fe19f7d2556c68059611
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-11-02 16:27:02 +0000

    1-print_numbers.c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -22,7 +22,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 		printf("%d", num);
 
-		if (separator != NULL)
+		if (separator != NULL && (i != n - 1))
 		{
 			printf("%s", separator);
 		}
