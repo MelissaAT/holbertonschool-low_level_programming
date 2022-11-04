32e3ae33168a243beb087dfe8719789653285257
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-11-04 17:55:27 +0000

    calc

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -19,3 +19,15 @@ int (*get_op_func(char *s))(int, int)
         {NULL, NULL}
     };
     int i;
+
+	i = 0;
+	while (i < 5)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,10 +1,45 @@
-#include "3-calc.h"
+rrrrlude "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
 /**
  * main - Entry point
+ * @argc: argument cout
+ * @argv: array of arguments
  * Return: .
  */
 int main(int argc, char *argv[])
 {
-	int (*array[])(int, int) = {
+	int a, b, c;
+	char *s;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (*argv[3] == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	s = argv[2];
+
+	if (*s == '+' || *s == '-' || *s == '/' || *s == '*' || *s == '%')
+	{
+		c = get_op_func(s)(a, b);
+
+		printf("%d\n", c);
+	}
+	else
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	return (0);
+}
