0eaf49adb8c6d97691accfbc9102938d9a6bc490
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-11-15 01:35:29 +0000

    adding new files

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * print_list - prints elements of a list
+ * @h: parameter
+ * Return: the numbers of the node
+ */
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		printf("[%d]", h->len);
+		i++;
+	if (h->str == NULL)
+	{
+		printf("(nil)\n");
+	}
+	else
+	{
+		printf("%s\n", h->str);
+	}
+		h = h->next;
+	}
+	return (i);
+}
