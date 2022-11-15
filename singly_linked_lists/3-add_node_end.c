d923af294c55448ea7f19dfd6d88ce47cd3183f1
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-11-15 21:27:03 +0000

    3-add_node

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -3,12 +3,13 @@
  * add_node_end - adds node st the end
  * @head: double pointer
  * @str: string
+ * Return: Succes
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *temp = malloc(sizeof(list_t));
 	list_t *last = *head;
-	
+
 	temp->str = strdup(str);
 	temp->len = strlen(str);
 	temp->next = NULL;
@@ -16,7 +17,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (*head == NULL)
 	{
 		*head = temp;
-		return (NULL);
+		return (*head);
 	}
 
 	while (last->next != NULL)
