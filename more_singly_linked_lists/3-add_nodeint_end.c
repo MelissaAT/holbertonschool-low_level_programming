5acc432b38cd7c20df763d846caa320942b7a8e2
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-11-16 03:00:43 +0000

    3-add_nodeint_end.c

---
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - adds node st the end
+ * @head: double pointer
+ * @n: int
+ * Return: Succes
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *temp, *last;
+
+	temp = malloc(sizeof(listint_t));
+
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
+	temp->n = n;
+	temp->next = NULL;
+
+	if (head == NULL)
+	{
+		*head = temp;
+		return (temp);
+	}
+	last = *head;
+
+	while (last->next != NULL)
+	{
+		last = last->next;
+	}
+	last->next = temp;
+	return (temp);
+}
