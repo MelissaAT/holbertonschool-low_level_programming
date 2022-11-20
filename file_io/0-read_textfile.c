f4c866e05b8b52ea7c030d26334a4a5db29b5f51
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-11-20 03:08:18 +0000

    0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -7,4 +7,29 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	ssize_t i;
+	char *fp;
+
+	fp = malloc(sizeof(fp));
+	
+	if (fp == NULL)
+	{
+		return (0);
+	}
+
+	if (filename == NULL)
+	{
+	   return (0);
+	}
+
+
+	i = open(filename, O_RDONLY);
+	while (read(STDIN_FILENO, &letters, 1) != 0)
+		write(STDOUT_FILENO, &i, 1);
+
+	close(*fp);
+
+	return (i);
+}
+
 
