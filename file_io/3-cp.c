c698c182faa3b342c09299c345ed0a34768557ad
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-11-20 16:48:07 +0000

    3-cp.c

---
+++ file_io/3-cp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+void main()
+{
+	FILE *fp1 = NULL, *fp2 = NULL;
+	char c;
+
+	fp1 = fopen("file_from", "r");
+	if (fp1 == NULL)
+	{
+		printf("Usage: cp file_from file_to\n");
+			exit(97);
+	}
+	fp2 = fopen("file_to", "w");
+	if (fp2 == NULL)
+	{
+		printf("Error: Can't read from file\n");
+			exit(98);
+	}
+	while ((c = fgetc(fp1)) != EOF)
+	{
+		fputc(c, fp2);
+	}
+	fclose(fp1);
+	fclose(fp2);
+}
