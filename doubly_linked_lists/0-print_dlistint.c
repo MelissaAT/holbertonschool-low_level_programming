77a2b4efc69704e5931945e8a9100e19b25643c7
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-11-21 14:53:44 +0000

    0-print_dlistint.c

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 /**
- * print_distint - prints all elemnts os a list
+ * print_dlistint - prints all elemnts os a list
  * @h: pointer
  * Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i = 0;
-	
+
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
