a3af8f1547b85b4b9ad051cb87177df7ed8d466f
MelissaAT <melissaarroyotorres9806@gmail.com>
2022-11-21 16:22:11 +0000

    .

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -15,7 +15,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	while (temp != NULL && i < (index - 1))
 	{
-		temp - temp->next;
+		temp = temp->next;
 		i++;
 	}
 	deletenode = temp->next;
