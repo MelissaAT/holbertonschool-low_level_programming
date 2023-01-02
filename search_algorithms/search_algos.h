b842400a4995cc75ad863c6dfa7602e6e4c0af3a
MelissaAT <melissaarroyotorres9806@gmail.com>
2023-01-02 21:17:54 +0000

    added files and first exercise

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,7 +1,7 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 #include <stdio.h>
-#include <stdilib.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stddef.h>
 int linear_search(int *array, size_t size, int value);
