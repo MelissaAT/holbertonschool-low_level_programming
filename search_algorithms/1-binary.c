f72a3a498678470be48ef3d8e7af58966e508036
MelissaAT <melissaarroyotorres9806@gmail.com>
2023-01-04 14:05:01 +0000

    1-binary.c

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -42,11 +42,11 @@ int binary_search(int *array, size_t size, int value)
 		}
 		else if (array[mid] < value)
 		{
-			start = mid + 1;
+			end = mid - 1;
 		}
 		else
 		{
-			end = mid - 1;
+			start = mid + 1;
 		}
 
 	return (-1);
